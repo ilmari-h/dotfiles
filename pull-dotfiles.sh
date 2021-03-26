4902fcbd7926b50f46bd0df6a2c1d3e36381e5a7
ilmari <7689734-_ilmari@users.noreply.gitlab.com>
2021-03-26 20:58:13 +0200

    Remove unecessary sed from pull-dotiles.sh

--- pull-dotfiles.sh
+++ pull-dotfiles.sh
@@ -2,5 +2,4 @@
 
 # Everything but git-related. Exclude folders to only get explicit files.
 find ./ -type f ! -path '*/.git*' -print0  \
-  | sed -r "s/^\s*.//" \
   | rsync --exclude './' --files-from=- --from0 $HOME ./
