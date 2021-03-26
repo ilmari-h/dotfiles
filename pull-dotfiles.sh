01348a870b93b58e4353d85096aa0a523650041b
ilmari <7689734-_ilmari@users.noreply.gitlab.com>
2021-03-26 20:23:50 +0200

    Add dotfiles sync script, initial.

---
+++ pull-dotfiles.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Everything but git-related. Exclude folders to only get explicit files.
+find ./ -type f ! -path '*/.git*' -print0  \
+  | sed -r "s/^\s*.//" \
+  | rsync --exclude './' --files-from=- --from0 $HOME ./test
